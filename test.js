cc6122fc83b6a36f58531cecf1a63fdac8217172
Shubham <shubhamthori99@gmail.com>
2022-10-04 12:24:10 +0530

    another commit

--- test.js
+++ test.js
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+const num1 = 5;
+const num2 = 3;
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+// add two numbers
+const sum = num1 + num2;
 
-<body>
-    <script>
-        function bubbleSort(array) {
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+function bubbleSort(array) {
             var done = false;
             while (!done) {
                 done = true;
@@ -30,8 +27,3 @@
         var numbers = [12, 10, 15, 11, 14, 13, 16];
         bubbleSort(numbers);
         console.log(numbers);
-    </script>
-
-</body>
-
-</html>
